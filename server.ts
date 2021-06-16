32eda4a9b5f6ab75e28fde4db992a29fce0a462f
biomiller <bt17001@qmul.ac.uk>
2021-06-16 19:39:37 +0100

    Added check for valid provider in request body

--- server.ts
+++ server.ts
@@ -1,21 +1,33 @@
 import express from "express"
-import cors from 'cors';
+import cors from "cors";
 import axios from "axios"
+import bodyParser from "body-parser";
 
 const port = 3001;
 const datahogURL = "http://localhost:3000/providers/";
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json())
 
 app.listen(port, () => console.log(`App running on port ${port}`))
 
 app.post("/getData", (req, res, next) => {
 
-    let provider = req.body.provider;
-    
-    axios
-        .get(datahogURL + provider)
-        .then(response => res.json(response.data))
-        .catch (error => next(error))
+    const acceptedProviders = [
+        "gas", 
+        "internet",
+    ]
+    let provider;
+
+    if(acceptedProviders.includes(req.body.provider)){
+        provider = req.body.provider;
+        axios
+            .get(datahogURL + provider)
+            .then(response => res.json(response.data))
+            .catch (error => next(error))
+    } else {
+        res.send("Invalid provider supplied");
+    }
+
 });
